2078a47b05e8a9feb439f89c387570195db766a4
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-24 17:39:48 -0700

    Add event handling to place markers on map on click event.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+var markers = [];
+
 function initialize() {
+  
   var mapOptions = {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   };
+  
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  }
+  
+  google.maps.event.addListener(map, 'click', function(event){
+    markers.push(new google.maps.Marker({
+      position: event.latLng,
+      map: map
+    }));
+  });
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
