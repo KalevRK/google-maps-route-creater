84151cc7cc04082678194a2a8e464daf83dd59d1
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-24 18:14:54 -0700

    Generate route between two markers placed on the map.

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+var map;
 var markers = [];
 
 function initialize() {
   
   var mapOptions = {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 37.770, lng: -122.480},
+    zoom: 13
   };
   
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   
+  directionsDisplay.setMap(map);
+
   google.maps.event.addListener(map, 'click', function(event){
     markers.push(new google.maps.Marker({
       position: event.latLng,
       map: map
     }));
   });
+
+  google.maps.event.addDomListener(document.getElementById('generate'), 'click', generateRoute);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function generateRoute() {
+
+  console.log(markers[0].getPosition().lat());
+  console.log(markers[0].getPosition().lng());
+  console.log(markers[1].getPosition().lat());
+  console.log(markers[1].getPosition().lng());
+
+  var beginning = new google.maps.LatLng(markers[0].getPosition().lat(),markers[0].getPosition().lng());
+  var end = new google.maps.LatLng(markers[1].getPosition().lat(),markers[1].getPosition().lng());
+
+  var request = {
+    origin: beginning,
+    destination: end,
+    travelMode: google.maps.TravelMode.WALKING
+  };
+
+  console.log(request);
+
+  directionsService.route(request, function(response, status) {
+    console.log('status:', status);
+    console.log('response:', response);
+    if (status == 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      console.error('Directions request failed');
+    }
+  });
+}
